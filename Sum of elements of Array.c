1c004b72620898964bac779addc62c933300eb87
Shivam Gupta <shivam4128@gmail.com>
2023-02-08 00:31:36 +0530

    sum_of_elements: add newline

--- Sum of elements of Array.c
+++ Sum of elements of Array.c
@@ -9,6 +9,7 @@ int main() {
     scanf("%d",&n);
     int *arr = (int*)malloc(n * sizeof(int));
     printf("Enter the elements of the array :");
+    
     for(i=0; i<n; i++)
         scanf("%d",&arr[i]);
 
